08c8ba84281be4ee6c1e1d9e2a581d7f872f92ce
Anton Polenyaka <antonpolenyaka@sitel-sa.com>
2019-04-03 09:38:43 +0200

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,14 @@
+"use strict";
+
 function sum(a, b) {
-  /* ваш код */
+  if(typeof(a) != "number") {
+    throw new TypeError("Value 'a' is not a number");
+  }
+  if(typeof(b) != "number") {
+    throw new TypeError("Value 'b' is not a number");
+  }
+  const result = a + b;
+  return result;
 }
 
 module.exports = sum;
